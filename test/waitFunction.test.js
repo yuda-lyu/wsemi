b1783704520405f8e5d49b9f460693ed0eea7032
yuda-lyu <firsemisphere@gmail.com>
2019-05-17 14:59:40 +0800

    modify waitFunction test

--- test/waitFunction.test.js
+++ test/waitFunction.test.js
@@ -4,12 +4,12 @@ import waitFunction from '../src/waitFunction.mjs'
 
 describe('waitFunction', function() {
 
-    let i = 0
-    let fn = function() {
-        i++
-        return i >= 2
-    }
     it(`should touch [then] in 2 sec. when call waitFunction`, function() {
+        let i = 0
+        let fn = function() {
+            i++
+            return i >= 2
+        }
         waitFunction(fn)
             .then(function() {
                 assert.strict.deepEqual(1, 1)
