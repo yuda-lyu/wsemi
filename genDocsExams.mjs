6143576422024cab65740d51ce1af9ef9cd1ddd4
yuda-lyu <firsemisphere@gmail.com>
2019-05-18 10:21:25 +0800

    modify genDocsExams.mjs

--- genDocsExams.mjs
+++ genDocsExams.mjs
@@ -2,16 +2,11 @@ import _ from 'lodash'
 import fs from 'fs'
 
 
-//let fn_pks = 'package.json'
 let fn_rdme = 'README.md'
 let fd_src = './examples/'
 let fd_tar = './docs/examples/'
 
 
-// function getPks(fn) {
-//     let c = fs.readFileSync(fn, 'utf8')
-//     return JSON.parse(c)
-// }
 function getWSemiCDN(fn) {
     let c = fs.readFileSync(fn, 'utf8')
     let s = _.split(c, '\r\n')
@@ -31,13 +26,8 @@ async function getFiles(fd) {
 
 async function main() {
 
-    // //pks
-    // let pks = getPks(fn_pks)
-    // console.log(pks)
-
     //cdn
     let cdn = getWSemiCDN(fn_rdme)
-    //console.log(cdn)
 
     //mkdirSync
     fs.mkdirSync(fd_tar)
@@ -59,9 +49,7 @@ async function main() {
 
         //write content
         //console.log(c)
-        //console.log(fd_tar + v)
         fs.writeFileSync(fd_tar + v, c, 'utf8')
-
 
     })
 
