bcf3246ae4dc46602f3433c7a530f24e1349966c
yuda-lyu <firsemisphere@gmail.com>
2019-05-19 20:12:43 +0800

    modify window detecter

--- src/domTriggerEvent.mjs
+++ src/domTriggerEvent.mjs
@@ -1,7 +1,7 @@
-//MouseEvent polyfill
-//https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/MouseEvent#Polyfill
-
 function init() {
+    //MouseEvent polyfill
+    //https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/MouseEvent#Polyfill
+
     try {
         new MouseEvent('test')
         return false // No need to polyfill
@@ -41,49 +41,6 @@ function init() {
 if (typeof window !== 'undefined') {
     init()
 }
-// (function (window) {
-
-//     //check
-//     if (!window) {
-//         return
-//     }
-
-//     try {
-//         new MouseEvent('test')
-//         return false // No need to polyfill
-//     }
-//     catch (e) {
-//         // Need to polyfill - fall through
-//     }
-
-//     // Polyfills DOM4 MouseEvent
-//     let MouseEventPolyfill = function (eventType, params) {
-//         params = params || { bubbles: false, cancelable: false }
-//         let mouseEvent = document.createEvent('MouseEvent')
-//         mouseEvent.initMouseEvent(eventType,
-//             params.bubbles,
-//             params.cancelable,
-//             window,
-//             0,
-//             params.screenX || 0,
-//             params.screenY || 0,
-//             params.clientX || 0,
-//             params.clientY || 0,
-//             params.ctrlKey || false,
-//             params.altKey || false,
-//             params.shiftKey || false,
-//             params.metaKey || false,
-//             params.button || 0,
-//             params.relatedTarget || null
-//         )
-
-//         return mouseEvent
-//     }
-
-//     MouseEventPolyfill.prototype = Event.prototype
-
-//     window.MouseEvent = MouseEventPolyfill
-// })(window)
 
 
 /**
