f871504d18a51f9cd8c49cc9f12561666d8a35df
yuda-lyu <firsemisphere@gmail.com>
2019-11-13 16:42:28 +0800

    modify: add domDropFiles

--- README.md
+++ README.md
@@ -87,5 +87,5 @@ or
 ```
 [Necessary] Add script for wsemi.
 ```alias
-<script src="https://cdn.jsdelivr.net/npm/wsemi@1.2.46/dist/wsemi.umd.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/wsemi@1.2.47/dist/wsemi.umd.js"></script>
 ```
---
+++ src/domDropFiles.mjs
@@ -0,0 +1,86 @@
+import Mitt from 'mitt'
+
+
+/**
+ * 前端取得拖曳進指定元素的檔案陣列
+ *
+ * Unit Test: {@link https://github.com/yuda-lyu/wsemi/blob/master/test/domAppend.test.js Github}
+ * @memberOf wsemi
+ * @param {HTMLElement} ele 輸入dom元素
+ * @returns {Object} 回傳物件，包含emit與off事件，emit可監聽dropIn、dropOut、getFiles，通過監聽getFiles即可取得拖曳進指定元素的檔案陣列
+ * @example
+ * need test in browser
+ */
+function domDropFiles(ele) {
+    let bMouseIn = false
+    let bMouseInTemp = false
+
+    //ev
+    let ev = new Mitt()
+
+    //addEventListener
+    ele.addEventListener('dragenter', dgDragin, false)
+    ele.addEventListener('dragover', dgDragin, false)
+    ele.addEventListener('dragleave', dgDragout, false)
+    ele.addEventListener('drop', dgDrop, false)
+
+    function dgIn(e) {
+        e.stopPropagation()
+        e.preventDefault()
+        bMouseInTemp = true //所有滑鼠進入或移動都標記為滑鼠移入狀態
+        if (bMouseIn === false) {
+            bMouseIn = true
+            ev.emit('dropIn', e)
+        }
+    }
+
+    function dgOut(e) {
+        e.stopPropagation()
+        e.preventDefault()
+        bMouseInTemp = false //暫時視為滑鼠移出
+        setTimeout(function() {
+            if (!bMouseInTemp) { //若100ms後仍為滑鼠移出狀態才emit事件
+                bMouseIn = false
+                ev.emit('dropOut', e)
+            }
+        }, 100)
+    }
+
+    function dgDragin(e) {
+        dgIn(e)
+    }
+
+    function dgDragout(e) {
+        dgOut(e)
+    }
+
+    function dgDrop(e) {
+
+        //dgIn
+        dgIn(e)
+
+        //cb for dgOut
+        function cb() {
+            dgOut(e)
+        }
+
+        //emit
+        let files = e.dataTransfer.files
+        ev.emit('getFiles', { files, e, cb })
+
+    }
+
+    //off
+    function off() {
+        ele.removeEventListener('dragenter', dgDragin, false)
+        ele.removeEventListener('dragover', dgDragin, false)
+        ele.removeEventListener('dragleave', dgDragout, false)
+        ele.removeEventListener('drop', dgDrop, false)
+    }
+    ev.off = off
+
+    return ev
+}
+
+
+export default domDropFiles
