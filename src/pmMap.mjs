53d134d647a85c745679c0a9314fdaa20e17cf49
yuda-lyu <firsemisphere@gmail.com>
2020-01-02 10:31:42 +0800

    modify: jsdoc

--- src/pmMap.mjs
+++ src/pmMap.mjs
@@ -9,9 +9,11 @@ import queue from './queue.mjs'
 
 /**
  * Promise的map，可設定同時處理數量。
- * 若先行產生promise則takeLimit設定會無效，因已先行初始化啟動。
- * 等同於Bluebird的Promise.map，而concurrency為takeLimit。
  * 若輸入rs為資料陣列則fn需將數據處理並回傳Promise，若輸入rs為Promise陣列則fn可不給，並循序執行各Promise。
+ * 若先行產生promise則takeLimit設定會無效，因promise已先行初始化啟動。
+ * 等同於Bluebird的Promise.map，而concurrency為takeLimit。
+ * 使用函數執行時，當takeLimit=1，等同於mapSeries，promise依序執行完畢才會調用下一個promise。
+ * 使用函數執行時，當takeLimit=0，等同於先行產生promise的情形，各promise直接執行視各自執行時間結束。
  *
  * Unit Test: {@link https://github.com/yuda-lyu/wsemi/blob/master/test/pmMap.test.js Github}
  * @memberOf wsemi
@@ -21,12 +23,13 @@ import queue from './queue.mjs'
  * @returns {Promise} 回傳Promise，resolve為成功結果，reject為失敗結果
  * @example
  *
- * let takeLimit = 2
+ * let takeLimit
  *
  * setTimeout(function() {
  *
- *     //通過function調用產生promise, 可受takeLimit控管同時執行數量
- *     pmMap([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], function (v, k) {
+ *     //通過function調用產生promise, 可受takeLimit控管同時執行數量, takeLimit=2
+ *     takeLimit = 2
+ *     pmMap([1, 2, 3, 4, 5], function (v, k) {
  *         console.log('call', v)
  *         return new Promise(function(resolve, reject) {
  *             setTimeout(function() {
@@ -37,6 +40,7 @@ import queue from './queue.mjs'
  *     }, takeLimit)
  *         .then(function(res) {
  *             console.log('use function then', JSON.stringify(res))
+ *             console.log('')
  *         })
  *         .catch(function(err) {
  *             console.log('use function catch', err)
@@ -46,8 +50,55 @@ import queue from './queue.mjs'
  *
  * setTimeout(function() {
  *
+ *     //通過function調用產生promise, takeLimit=1, 等同於mapSeries
+ *     takeLimit = 1
+ *     pmMap([1, 2, 3, 4, 5], function (v, k) {
+ *         console.log('call', v)
+ *         return new Promise(function(resolve, reject) {
+ *             setTimeout(function() {
+ *                 console.log('use function resolve', v)
+ *                 resolve('#' + v)
+ *             }, 300 - v * 10)
+ *         })
+ *     }, takeLimit)
+ *         .then(function(res) {
+ *             console.log('use function then', JSON.stringify(res))
+ *             console.log('')
+ *         })
+ *         .catch(function(err) {
+ *             console.log('use function catch', err)
+ *         })
+ *
+ * }, 1500)
+ *
+ * setTimeout(function() {
+ *
+ *     //通過function調用產生promise, takeLimit=0, 無限制同時執行數量
+ *     takeLimit = 0
+ *     pmMap([1, 2, 3, 4, 5], function (v, k) {
+ *         console.log('call', v)
+ *         return new Promise(function(resolve, reject) {
+ *             setTimeout(function() {
+ *                 console.log('use function resolve', v)
+ *                 resolve('#' + v)
+ *             }, 300 - v * 10)
+ *         })
+ *     }, takeLimit)
+ *         .then(function(res) {
+ *             console.log('use function then', JSON.stringify(res))
+ *             console.log('')
+ *         })
+ *         .catch(function(err) {
+ *             console.log('use function catch', err)
+ *         })
+ *
+ * }, 3000)
+ *
+ * setTimeout(function() {
+ *
  *     //先產生promise, 因事先初始化故無法受takeLimit控管
- *     let rs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(function(v, k) {
+ *     takeLimit = 2
+ *     let rs = [1, 2, 3, 4, 5].map(function(v, k) {
  *         console.log('call', v)
  *         return new Promise(function(resolve, reject) {
  *             setTimeout(function() {
@@ -59,33 +110,13 @@ import queue from './queue.mjs'
  *     pmMap(rs, null, takeLimit)
  *         .then(function(res) {
  *             console.log('use promise then', JSON.stringify(res))
+ *             console.log('')
  *         })
  *         .catch(function(err) {
  *             console.log('use promise catch', err)
  *         })
  *
- * }, 3000)
- *
- * setTimeout(function() {
- *
- *     //通過function調用產生promise, 使用takeLimit=0也就是無限制同時執行數量
- *     pmMap([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], function (v, k) {
- *         console.log('call', v)
- *         return new Promise(function(resolve, reject) {
- *             setTimeout(function() {
- *                 console.log('use function resolve', v)
- *                 resolve('#' + v)
- *             }, 300 - v * 10)
- *         })
- *     })
- *         .then(function(res) {
- *             console.log('use function then', JSON.stringify(res))
- *         })
- *         .catch(function(err) {
- *             console.log('use function catch', err)
- *         })
- *
- * }, 6000)
+ * }, 4500)
  *
  * // call 1
  * // call 2
@@ -93,65 +124,47 @@ import queue from './queue.mjs'
  * // call 3
  * // use function resolve 1
  * // call 4
+ * // use function resolve 3
+ * // call 5
+ * // use function resolve 4
+ * // use function resolve 5
+ * // use function then ["#1","#2","#3","#4","#5"]
+ *
+ * // call 1
+ * // use function resolve 1
+ * // call 2
+ * // use function resolve 2
+ * // call 3
+ * // use function resolve 3
+ * // call 4
  * // use function resolve 4
  * // call 5
- * // use function resolve 3
- * // call 6
- * // use function resolve 6
- * // call 7
  * // use function resolve 5
- * // call 8
- * // use function resolve 8
- * // call 9
- * // use function resolve 7
- * // call 10
- * // use function resolve 10
- * // use function resolve 9
- * // use function then ["#1","#2","#3","#4","#5","#6","#7","#8","#9","#10"]
+ * // use function then ["#1","#2","#3","#4","#5"]
  *
  * // call 1
  * // call 2
  * // call 3
  * // call 4
  * // call 5
- * // call 6
- * // call 7
- * // call 8
- * // call 9
- * // call 10
- * // use promise resolve 10
- * // use promise resolve 9
- * // use promise resolve 8
- * // use promise resolve 7
- * // use promise resolve 6
+ * // use function resolve 5
+ * // use function resolve 4
+ * // use function resolve 3
+ * // use function resolve 2
+ * // use function resolve 1
+ * // use function then ["#1","#2","#3","#4","#5"]
+ *
+ * // call 1
+ * // call 2
+ * // call 3
+ * // call 4
+ * // call 5
  * // use promise resolve 5
  * // use promise resolve 4
  * // use promise resolve 3
  * // use promise resolve 2
  * // use promise resolve 1
- * // use promise then ["#1","#2","#3","#4","#5","#6","#7","#8","#9","#10"]
- *
- * // call 1
- * // call 2
- * // call 3
- * // call 4
- * // call 5
- * // call 6
- * // call 7
- * // call 8
- * // call 9
- * // call 10
- * // use function resolve 10
- * // use function resolve 9
- * // use function resolve 8
- * // use function resolve 7
- * // use function resolve 6
- * // use function resolve 5
- * // use function resolve 4
- * // use function resolve 3
- * // use function resolve 2
- * // use function resolve 1
- * // use function then ["#1","#2","#3","#4","#5","#6","#7","#8","#9","#10"]
+ * // use promise then ["#1","#2","#3","#4","#5"]
  *
  */
 function pmMap(rs, fn, takeLimit = 0) {
--- src/pmSeries.mjs
+++ src/pmSeries.mjs
@@ -7,6 +7,8 @@ import isfun from './isfun.mjs'
 /**
  * Promise的mapSeries，循序執行promise。
  * 若輸入rs為資料陣列則fn需將數據處理並回傳Promise，若輸入rs為Promise陣列則fn可不給，並循序執行各Promise。
+ * 使用函數執行時，等同於pmMap使用函數執行且takeLimit=1，promise依序執行完畢才會調用下一個promise。
+ * 先行產生promise時，等同於pmMap使用函數執行且takeLimit=0，或是等同於pmMap先行產生promise，各promise直接執行視各自執行時間結束。
  *
  * Unit Test: {@link https://github.com/yuda-lyu/wsemi/blob/master/test/pmSeries.test.js Github}
  * @memberOf wsemi
@@ -15,45 +17,74 @@ import isfun from './isfun.mjs'
  * @returns {Promise} 回傳Promise，resolve為成功結果，reject為失敗結果
  * @example
  *
- * pmSeries([2, 3, 1], function(v, k) {
- *     return new Promise(function(resolve, reject) {
- *         setTimeout(function() {
- *             console.log('use function resolve: ' + v)
- *             resolve('use function: ' + v + '(' + k + ')')
- *         }, 1)
- *     })
- * })
- *     .then(function(r) {
- *         console.log('use function then', r)
- *     })
- *     .catch(function(err) {
- *         console.log('use function catch', err)
- *     })
+ * setTimeout(function() {
  *
- * let rs = [2, 3, 1].map(function(v, k) {
- *     return new Promise(function(resolve, reject) {
- *         setTimeout(function() {
- *             console.log('use promise resolve: ' + v)
- *             resolve('use promise: ' + v + '(' + k + ')')
- *         }, 1)
+ *     //通過function調用產生promise, 各promise循序調用
+ *     pmSeries([1, 2, 3, 4, 5], function (v, k) {
+ *         console.log('call', v)
+ *         return new Promise(function(resolve, reject) {
+ *             setTimeout(function() {
+ *                 console.log('use function resolve', v)
+ *                 resolve('#' + v)
+ *             }, 300 - v * 10)
+ *         })
  *     })
- * })
- * pmSeries(rs)
- *     .then(function(r) {
- *         console.log('use promise then', r)
- *     })
- *     .catch(function(err) {
- *         console.log('use promise catch', err)
- *     })
+ *         .then(function(res) {
+ *             console.log('use function then', JSON.stringify(res))
+ *             console.log('')
+ *         })
+ *         .catch(function(err) {
+ *             console.log('use function catch', err)
+ *         })
  *
- * // use promise resolve: 2
- * // use promise resolve: 3
- * // use promise resolve: 1
- * // use promise then [ 'use promise: 2(0)', 'use promise: 3(1)', 'use promise: 1(2)' ]
- * // use function resolve: 2
- * // use function resolve: 3
- * // use function resolve: 1
- * // use function then [ 'use function: 2(0)', 'use function: 3(1)', 'use function: 1(2)' ]
+ * }, 1)
+ *
+ * setTimeout(function() {
+ *
+ *     //先產生promise, 因事先初始化故各promise會依照各自執行時間結束
+ *     let rs = [1, 2, 3, 4, 5].map(function(v, k) {
+ *         console.log('call', v)
+ *         return new Promise(function(resolve, reject) {
+ *             setTimeout(function() {
+ *                 console.log('use promise resolve', v)
+ *                 resolve('#' + v)
+ *             }, 300 - v * 10)
+ *         })
+ *     })
+ *     pmSeries(rs, null)
+ *         .then(function(res) {
+ *             console.log('use promise then', JSON.stringify(res))
+ *             console.log('')
+ *         })
+ *         .catch(function(err) {
+ *             console.log('use promise catch', err)
+ *         })
+ *
+ * }, 1500)
+ *
+ * // call 1
+ * // use function resolve 1
+ * // call 2
+ * // use function resolve 2
+ * // call 3
+ * // use function resolve 3
+ * // call 4
+ * // use function resolve 4
+ * // call 5
+ * // use function resolve 5
+ * // use function then ["#1","#2","#3","#4","#5"]
+ *
+ * // call 1
+ * // call 2
+ * // call 3
+ * // call 4
+ * // call 5
+ * // use promise resolve 5
+ * // use promise resolve 4
+ * // use promise resolve 3
+ * // use promise resolve 2
+ * // use promise resolve 1
+ * // use promise then ["#1","#2","#3","#4","#5"]
  *
  */
 function pmSeries(rs, fn) {
