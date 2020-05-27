61e867422a25fcc7f15b5a7d01a71401cf546997
yuda-lyu <firsemisphere@gmail.com>
2020-05-27 08:49:36 +0800

    fix: error result in getEnv test

--- dist/wsemi.umd.js
+++ dist/wsemi.umd.js
@@ -1,5 +1,5 @@
 /*!
- * wsemi v1.4.30
+ * wsemi v1.4.31
  * (c) 2018-2019 yuda-lyu(semisphere)
  * Released under the MIT License.
  */
--- test/getEnv.test.js
+++ test/getEnv.test.js
@@ -4,9 +4,9 @@ import getEnv from '../src/getEnv.mjs'
 
 describe(`getEnv`, function() {
 
-    it(`should return { isWindow: false, isWebWorker: false, isNode: true } in nodejs when no input`, function() {
+    it(`should return { isBrowser: false, isWebWorker: false, isNode: true } in nodejs when no input`, function() {
         let r = getEnv()
-        assert.strict.deepEqual(r, { isWindow: false, isWebWorker: false, isNode: true })
+        assert.strict.deepEqual(r, { isBrowser: false, isWebWorker: false, isNode: true })
     })
 
 })
