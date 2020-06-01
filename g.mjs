b255ddd7cdc233e5190ad60e7e5d5f8a7220ea78
yuda-lyu <firsemisphere@gmail.com>
2020-06-01 09:34:39 +0800

    modify: rename arrpull to ltdtpull

--- g.mjs
+++ g.mjs
@@ -1,12 +1,8 @@
-import arradd from './src/arradd.mjs'
+import ltdtpull from './src/ltdtpull.mjs'
 
-let v1 = [1, 2, 3, 4]
-let v2 = [0.1, 0.1, 0.1, 0.1]
-let v3 = [11, 22, 33, 44]
-console.log(arradd(v1, v2))
-// => [ 1.1, 2.1, 3.1, 4.1 ]
-console.log(arradd(v1, v2, v3))
-// => [ 12.1, 24.1, 36.1, 48.1 ]
-
+console.log(ltdtpull([{ x: 1, y: 'y1' }, { x: 2, y: 'y2' }], [{ x: 1, y: 'y3' }], 'x'))
+// => [ { x: 2, y: 'y2' } ]
+console.log(ltdtpull([{ id: 1, v: 'v1' }, { id: 2, v: 'v2' }, { id: 3, v: 'v3' }], [{ id: 1, v: '-v1' }, { id: 3, v: '-v3' }], 'id'))
+// => [ { id: 2, v: 'v2' } ]
 //node --experimental-modules --es-module-specifier-resolution=node g.mjs
 
--- test/ltdtpull.test.js
+++ test/ltdtpull.test.js
@@ -1,125 +1,113 @@
 import assert from 'assert'
-import arrpull from '../src/arrpull.mjs'
+import ltdtpull from '../src/ltdtpull.mjs'
 
 
-describe(`arrpull`, function() {
+describe(`ltdtpull`, function() {
 
     it(`should return [ { x: 2, y: 'y2' } ] when input [{ x: 1, y: 'y1' }, { x: 2, y: 'y2' }], [{ x: 1, y: 'y3' }], 'x'`, function() {
-        let r = arrpull([{ x: 1, y: 'y1' }, { x: 2, y: 'y2' }], [{ x: 1, y: 'y3' }], 'x')
+        let r = ltdtpull([{ x: 1, y: 'y1' }, { x: 2, y: 'y2' }], [{ x: 1, y: 'y3' }], 'x')
         let rr = [{ x: 2, y: 'y2' }]
         assert.strict.deepEqual(r, rr)
     })
 
-    it(`should return [1, '3', 4, '3', 'abc'] when input [1, 2, '3', 4, '3', 'abc'], [2]`, function() {
-        let r = arrpull([1, 2, '3', 4, '3', 'abc'], [2])
-        let rr = [1, '3', 4, '3', 'abc']
+    it(`should return [ { id: 2, v: 'v2' } ] when input [{ id: 1, v: 'v1' }, { id: 2, v: 'v2' }, { id: 3, v: 'v3' }], [{ id: 1, v: '-v1' }, { id: 3, v: '-v3' }], 'id'`, function() {
+        let r = ltdtpull([{ id: 1, v: 'v1' }, { id: 2, v: 'v2' }, { id: 3, v: 'v3' }], [{ id: 1, v: '-v1' }, { id: 3, v: '-v3' }], 'id')
+        let rr = [{ id: 2, v: 'v2' }]
         assert.strict.deepEqual(r, rr)
     })
 
-    it(`should return [1, 4, 'abc'] when input [1, 2, '3', 4, '3', 'abc'], [2, '3']`, function() {
-        let r = arrpull([1, 2, '3', 4, '3', 'abc'], [2, '3'])
-        let rr = [1, 4, 'abc']
-        assert.strict.deepEqual(r, rr)
-    })
-
-    it(`should return [1, 4] when input [1, 2, '3', 4, '3', 'abc'], [2, '3', 'abc']`, function() {
-        let r = arrpull([1, 2, '3', 4, '3', 'abc'], [2, '3', 'abc'])
-        let rr = [1, 4]
-        assert.strict.deepEqual(r, rr)
-    })
-
-    it(`should return ['abc'] when input ['abc'], [1]`, function() {
-        let r = arrpull(['abc'], [1])
-        let rr = ['abc']
+    it(`should return [ { id: 2, v: 'v2' } ] when input [{ id: 1, v: 'v1' }, { id: 2, v: 'v2' }, { id: 3, v: 'v3' }], [], 'id'`, function() {
+        let r = ltdtpull([{ id: 1, v: 'v1' }, { id: 2, v: 'v2' }, { id: 3, v: 'v3' }], [], 'id')
+        let rr = [{ id: 1, v: 'v1' }, { id: 2, v: 'v2' }, { id: 3, v: 'v3' }]
         assert.strict.deepEqual(r, rr)
     })
 
     it(`should return [] when input ''`, function() {
-        let r = arrpull('')
+        let r = ltdtpull('')
         let rr = []
         assert.strict.deepEqual(r, rr)
     })
 
     it(`should return [] when input []`, function() {
-        let r = arrpull([])
+        let r = ltdtpull([])
         let rr = []
         assert.strict.deepEqual(r, rr)
     })
 
     it(`should return [] when input {}`, function() {
-        let r = arrpull({})
+        let r = ltdtpull({})
         let rr = []
         assert.strict.deepEqual(r, rr)
     })
 
     it(`should return [] when input null`, function() {
-        let r = arrpull(null)
+        let r = ltdtpull(null)
         let rr = []
         assert.strict.deepEqual(r, rr)
     })
 
     it(`should return [] when input undefined`, function() {
-        let r = arrpull(undefined)
+        let r = ltdtpull(undefined)
         let rr = []
         assert.strict.deepEqual(r, rr)
     })
 
     it(`should return [] when input [], ''`, function() {
-        let r = arrpull([], '')
+        let r = ltdtpull([], '')
         let rr = []
         assert.strict.deepEqual(r, rr)
     })
 
     it(`should return [] when input [], []`, function() {
-        let r = arrpull([], [])
+        let r = ltdtpull([], [])
         let rr = []
         assert.strict.deepEqual(r, rr)
     })
 
     it(`should return [] when input [], {}`, function() {
-        let r = arrpull([], {})
+        let r = ltdtpull([], {})
         let rr = []
         assert.strict.deepEqual(r, rr)
     })
 
     it(`should return [] when input [], null`, function() {
-        let r = arrpull([], null)
+        let r = ltdtpull([], null)
         let rr = []
         assert.strict.deepEqual(r, rr)
     })
 
     it(`should return [] when input [], undefined`, function() {
-        let r = arrpull([], undefined)
+        let r = ltdtpull([], undefined)
         let rr = []
         assert.strict.deepEqual(r, rr)
     })
 
     it(`should return [] when input ['abc'], ''`, function() {
-        let r = arrpull(['abc'], '')
+        let r = ltdtpull(['abc'], '')
         let rr = []
         assert.strict.deepEqual(r, rr)
     })
 
     it(`should return [] when input ['abc'], []`, function() {
-        let r = arrpull(['abc'], [])
-        let rr = ['abc']
+        let r = ltdtpull(['abc'], [])
+        let rr = []
         assert.strict.deepEqual(r, rr)
     })
 
     it(`should return [] when input ['abc'], {}`, function() {
-        let r = arrpull(['abc'], {})
+        let r = ltdtpull(['abc'], {})
         let rr = []
         assert.strict.deepEqual(r, rr)
     })
 
     it(`should return [] when input ['abc'], null`, function() {
-        let r = arrpull(['abc'], null)
+        let r = ltdtpull(['abc'], null)
         let rr = []
         assert.strict.deepEqual(r, rr)
     })
 
     it(`should return [] when input ['abc'], undefined`, function() {
-        let r = arrpull(['abc'], undefined)
+        let r = ltdtpull(['abc'], undefined)
         let rr = []
         assert.strict.deepEqual(r, rr)
     })
