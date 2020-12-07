edab68e5b1909d9aa78d782cbc1ec40a40646da8
yuda-lyu <firsemisphere@gmail.com>
2020-12-07 15:26:36 +0800

    modify: add infor type in domAlert

--- src/domAlert.mjs
+++ src/domAlert.mjs
@@ -9,6 +9,7 @@ import genPm from './genPm.mjs'
 import isestr from './isestr.mjs'
 import iseobj from './iseobj.mjs'
 import replace from './replace.mjs'
+import haskey from './haskey.mjs'
 import domFadeIn from './domFadeIn.mjs'
 import domFadeOut from './domFadeOut.mjs'
 
@@ -17,11 +18,13 @@ import domFadeOut from './domFadeOut.mjs'
 let iconSuccess = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 426.667 426.667" style="width:24px; height:24px; enable-background:new 0 0 426.667 426.667" xml:space="preserve"><path style="fill:#6ac259" d="M213.333,0C95.518,0,0,95.514,0,213.333s95.518,213.333,213.333,213.333  c117.828,0,213.333-95.514,213.333-213.333S331.157,0,213.333,0z M174.199,322.918l-93.935-93.931l31.309-31.309l62.626,62.622 l140.894-140.898l31.309,31.309L174.199,322.918z"/></svg>`
 let iconError = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 496.158 496.158" style="width:24px; height:24px; enable-background:new 0 0 496.158 496.158" xml:space="preserve"><path style="fill:#e04f5f" d="M496.158,248.085c0-137.021-111.07-248.082-248.076-248.082C111.07,0.003,0,111.063,0,248.085  c0,137.002,111.07,248.07,248.082,248.07C385.088,496.155,496.158,385.087,496.158,248.085z"/><path style="fill:#fff" d="M277.042,248.082l72.528-84.196c7.91-9.182,6.876-23.041-2.31-30.951  c-9.172-7.904-23.032-6.876-30.947,2.306l-68.236,79.212l-68.229-79.212c-7.91-9.188-21.771-10.216-30.954-2.306  c-9.186,7.91-10.214,21.77-2.304,30.951l72.522,84.196l-72.522,84.192c-7.91,9.182-6.882,23.041,2.304,30.951  c4.143,3.569,9.241,5.318,14.316,5.318c6.161,0,12.294-2.586,16.638-7.624l68.229-79.212l68.236,79.212  c4.338,5.041,10.47,7.624,16.637,7.624c5.069,0,10.168-1.749,14.311-5.318c9.186-7.91,10.22-21.77,2.31-30.951L277.042,248.082z"/></svg>`
 let iconWarning = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 497.472 497.472" style="width:24px; height:24px; enable-background:new 0 0 497.472 497.472" xml:space="preserve"><g transform="matrix(1.25 0 0 -1.25 0 45)"><path style="fill:#ffcc4d" d="M24.374-357.857c-20.958,0-30.197,15.223-20.548,33.826L181.421,17.928     c9.648,18.603,25.463,18.603,35.123,0L394.14-324.031c9.671-18.603,0.421-33.826-20.548-33.826H24.374z"/><path style="fill:#231f20" d="M173.605-80.922c0,14.814,10.934,23.984,25.395,23.984c14.12,0,25.407-9.512,25.407-23.984 V-216.75c0-14.461-11.287-23.984-25.407-23.984c-14.461,0-25.395,9.182-25.395,23.984V-80.922z M171.489-289.056 c0,15.167,12.345,27.511,27.511,27.511c15.167,0,27.523-12.345,27.523-27.511c0-15.178-12.356-27.523-27.523-27.523 C183.834-316.579,171.489-304.234,171.489-289.056"/></g></svg>`
+let iconInfor = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 496.16 496.16" style="width:24px; height:24px; enable-background:new 0 0 496.158 496.158;" xml:space="preserve"><path d="m496.16 248.08c0-137.02-111.07-248.08-248.08-248.08-137.01 0-248.08 111.06-248.08 248.08 0 137 111.07 248.07 248.08 248.07 137.01 0 248.08-111.07 248.08-248.07z" fill="#25B7D3"/><g fill="#fff"><path d="m315.25 359.56c-1.387-2.032-4.048-2.755-6.27-1.702-24.582 11.637-52.482 23.94-57.958 25.015-0.138-0.123-0.357-0.348-0.644-0.737-0.742-1.005-1.103-2.318-1.103-4.015 0-13.905 10.495-56.205 31.192-125.72 17.451-58.406 19.469-70.499 19.469-74.514 0-6.198-2.373-11.435-6.865-15.146-4.267-3.519-10.229-5.302-17.719-5.302-12.459 0-26.899 4.73-44.146 14.461-16.713 9.433-35.352 25.41-55.396 47.487-1.569 1.729-1.733 4.314-0.395 6.228 1.34 1.915 3.825 2.644 5.986 1.764 7.037-2.872 42.402-17.359 47.557-20.597 4.221-2.646 7.875-3.989 10.861-3.989 0.107 0 0.199 4e-3 0.276 0.01 0.036 0.198 0.07 0.5 0.07 0.933 0 3.047-0.627 6.654-1.856 10.703-30.136 97.641-44.785 157.5-44.785 182.99 0 8.998 2.501 16.242 7.432 21.528 5.025 5.393 11.803 8.127 20.146 8.127 8.891 0 19.712-3.714 33.08-11.354 12.936-7.392 32.68-23.653 60.363-49.717 1.793-1.687 2.092-4.426 0.705-6.458z"/><path d="m314.28 76.672c-4.925-5.041-11.227-7.597-18.729-7.597-9.34 0-17.475 3.691-24.176 10.971-6.594 7.16-9.938 15.946-9.938 26.113 0 8.033 2.463 14.69 7.32 19.785 4.922 5.172 11.139 7.794 18.476 7.794 8.958 0 17.049-3.898 24.047-11.586 6.876-7.553 10.363-16.433 10.363-26.393 1e-3 -7.654-2.476-14.075-7.363-19.087z"/></g></svg>`
 let iconCancel = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 512.001 512.001" style="width:12px; height:12px; enable-background:new 0 0 512.001 512.001;" xml:space="preserve"><g><g><path d="M284.286,256.002L506.143,34.144c7.811-7.811,7.811-20.475,0-28.285c-7.811-7.81-20.475-7.811-28.285,0L256,227.717L34.143,5.859c-7.811-7.811-20.475-7.811-28.285,0c-7.81,7.811-7.811,20.475,0,28.285l221.857,221.857L5.858,477.859c-7.811,7.811-7.811,20.475,0,28.285c3.905,3.905,9.024,5.857,14.143,5.857c5.119,0,10.237-1.952,14.143-5.857L256,284.287l221.857,221.857c3.905,3.905,9.024,5.857,14.143,5.857s10.237-1.952,14.143-5.857c7.811-7.811,7.811-20.475,0-28.285L284.286,256.002z"/></g></g></svg>`
 let kpIcon = {
     success: iconSuccess,
     error: iconError,
     warning: iconWarning,
+    infor: iconInfor,
     cancel: iconCancel,
 }
 
@@ -196,13 +199,14 @@ function addItem({ id, position, time, pm }) {
  * @param {String} [opt.icon=''] 輸入圖標字串，可為html，預設依照type給予指定圖標，指定圖標詳見程式碼
  * @param {String} [opt.position='top-right'] 輸入位置字串，可使用'top-right'、'top-left'、'bottom-right'、'bottom-left'，預設'top-right'
  * @param {Number} [opt.time=4000] 輸入顯示時間長度數字，單位ms，預設4000
- * @param {String} [opt.paddingStyle='14px 26px 14px 14px'] 輸入內寬設定字串，預設'14px 26px 14px 14px'
+ * @param {Object} [opt.paddingStyle={top:14,right:26,bottom:14,left:14}] 輸入內寬設定物件，可輸入top、bottom、left、right，單位為px，預設{top:14,right:26,bottom:14,left:14}
  * @param {String} [opt.textColor='#000'] 輸入文字顏色字串，預設'#000'
  * @param {String} [opt.backgroundColor='#fff'] 輸入背景顏色字串，預設'#fff'
  * @param {Number} [opt.borderWidth=1] 輸入邊框寬度數字，單位px，預設1
  * @param {String} [opt.borderColor='#ebeef5'] 輸入邊框顏色字串，預設'#ebeef5'
  * @param {Number} [opt.borderRadius=8] 輸入邊框圓角數字，單位px，預設8
  * @param {String} [opt.shadowStyle='0 2px 12px 0 rgba(0,0,0,.1)'] 輸入陰影樣式字串，預設'0 2px 12px 0 rgba(0,0,0,.1)'
+ * @param {Object} [opt.closeIconShift={top:7,right:7}] 輸入關閉按鈕平移距離設定物件，可輸入top、bottom、left、right，單位為px，若需要位於右上角則需設定top與right，預設{top:7,right:7}
  * @returns {Promise} 回傳Promise，resolve代表彈窗關閉，無reject
  * @example
  * need test in browser
@@ -222,14 +226,18 @@ function domAlert(msg, opt = {}) {
     }
 
     //type
-    let type = get(opt, 'type')
-    if (type !== 'warning' && type !== 'error') {
+    let type = get(opt, 'type', 'success')
+    if (!haskey(kpIcon, type)) {
         type = 'success'
     }
 
     //icon
-    let icon = get(opt, 'icon')
-    if (!isestr(icon)) {
+    let icon = get(opt, 'icon', null)
+    if (isestr(icon)) {
+        //若為有效字串, 則優先使用icon
+    }
+    else {
+        //否則由type與kpIcon取得icon
         icon = kpIcon[type]
     }
 
@@ -249,8 +257,20 @@ function domAlert(msg, opt = {}) {
     }
 
     //paddingStyle
-    let paddingStyle = get(opt, 'paddingStyle', '14px 26px 14px 14px')
-    paddingStyle = replace(paddingStyle, ';', '')
+    let paddingStyle = get(opt, 'paddingStyle', null)
+    if (paddingStyle === null) {
+        paddingStyle = { top: 14, right: 26, bottom: 14, left: 14 }
+    }
+
+    //usePaddingStyle
+    let usePaddingStyle = ''
+    if (true) {
+        let top = get(paddingStyle, 'top', 0)
+        let bottom = get(paddingStyle, 'bottom', 0)
+        let left = get(paddingStyle, 'left', 0)
+        let right = get(paddingStyle, 'right', 0)
+        usePaddingStyle = `padding:${top}px ${right}px ${bottom}px ${left}px;`
+    }
 
     //textColor
     let textColor = get(opt, 'textColor', '#000')
@@ -274,6 +294,34 @@ function domAlert(msg, opt = {}) {
     let shadowStyle = get(opt, 'shadowStyle', '0 2px 12px 0 rgba(0,0,0,.1)')
     shadowStyle = replace(shadowStyle, ';', '')
 
+    //closeIconShift
+    let closeIconShift = get(opt, 'closeIconShift', null)
+    if (closeIconShift === null) {
+        closeIconShift = { top: 10, right: 10 }
+    }
+
+    //useCloseIconPosition
+    let useCloseIconPosition = ''
+    if (true) {
+        let top = get(closeIconShift, 'top', '')
+        let bottom = get(closeIconShift, 'bottom', '')
+        let left = get(closeIconShift, 'left', '')
+        let right = get(closeIconShift, 'right', '')
+        if (isNumber(top)) {
+            top = `top:${top}px;`
+        }
+        if (isNumber(bottom)) {
+            bottom = `bottom:${bottom}px;`
+        }
+        if (isNumber(left)) {
+            left = `left:${left}px;`
+        }
+        if (isNumber(right)) {
+            right = `right:${right}px;`
+        }
+        useCloseIconPosition = `position:absolute; ${top} ${right} ${bottom} ${left}`
+    }
+
     //id
     let id = `alt-${genID()}`
     let idCancel = `alt-${genID()}`
@@ -285,7 +333,7 @@ function domAlert(msg, opt = {}) {
         onmouseenter="ttWAlertFadeIn('${idCancel}')"
         onmouseleave="ttWAlertFadeOut('${idCancel}')"
     >
-        <div style="position:relative; padding:${paddingStyle};">
+        <div style="position:relative; ${usePaddingStyle}">
 
             <div style="display:flex; align-items:center;">
                 <div style="margin-right:10px;">
@@ -294,7 +342,7 @@ function domAlert(msg, opt = {}) {
                 <div>${msg}</div>
             </div>
 
-            <div style="position:absolute; top:5px; right:5px;">
+            <div style="${useCloseIconPosition}">
                 <div 
                 id="${idCancel}" 
                     style="display:flex; cursor:pointer; opacity:0;" 
