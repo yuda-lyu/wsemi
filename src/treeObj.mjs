35eaec45b768abd3f9c6f0696e31ac0a7672fedf
yuda-lyu <firsemisphere@gmail.com>
2020-12-30 22:23:38 +0800

    modify: add opt.force in treeObj

--- src/treeObj.mjs
+++ src/treeObj.mjs
@@ -4,7 +4,10 @@ import isBoolean from 'lodash/isBoolean'
 import isFunction from 'lodash/isFunction'
 import size from 'lodash/size'
 import set from 'lodash/set'
+import get from 'lodash/get'
 import slice from 'lodash/slice'
+import range from 'lodash/range'
+import cloneDeep from 'lodash/cloneDeep'
 import isarr from './isarr.mjs'
 import isarr0 from './isarr0.mjs'
 import isobj from './isobj.mjs'
@@ -12,83 +15,81 @@ import isobj0 from './isobj0.mjs'
 import isab from './isab.mjs'
 import isu8arr from './isu8arr.mjs'
 import isu16arr from './isu16arr.mjs'
+import isbol from './isbol.mjs'
 
 
-/**
- * 遍歷物件並回傳複製物件，類似JSON.stringify，但不會序列化成為字串
- *
- * Unit Test: {@link https://github.com/yuda-lyu/wsemi/blob/master/test/treeObj.test.js Github}
- * @memberOf wsemi
- * @param {*} data 輸入任意資料
- * @param {Function} [hookFun=null] 輸入攔截處理函數，預設null
- * @returns {*} 回傳複製後的任意資料
- * @example
- *
- * let data = {
- *     a: 123,
- *     b: 145.67,
- *     c: 'test中文1',
- *     d: true,
- *     e: function() {},
- *     f: [11, 'xyz', false, new Uint8Array([166, 197, 215])],
- *     g: {
- *         ga: 223,
- *         gb: 245.67,
- *         gc: 'test中文2',
- *         gd: new Uint8Array([66, 97, 115]),
- *     },
- * }
- * let r = treeObj(data, (value, key, nk) => {
- *     console.log('=>', value, key, nk)
- *     return value
- * })
- * console.log('r =>', r)
- * // => 123 a []
- * // => 145.67 b []
- * // => test中文1 c []
- * // => true d []
- * // => [Function: e] e []
- * // => [ 11, 'xyz', false, Uint8Array(3) [ 166, 197, 215 ] ] f []
- * // => 11 0 [ 'f' ]
- * // => xyz 1 [ 'f' ]
- * // => false 2 [ 'f' ]
- * // => Uint8Array(3) [ 166, 197, 215 ] 3 [ 'f' ]
- * // => {
- * //   ga: 223,
- * //   gb: 245.67,
- * //   gc: 'test中文2',
- * //   gd: Uint8Array(3) [ 66, 97, 115 ]
- * // } g []
- * // => 223 ga [ 'g' ]
- * // => 245.67 gb [ 'g' ]
- * // => test中文2 gc [ 'g' ]
- * // => Uint8Array(3) [ 66, 97, 115 ] gd [ 'g' ]
- * // r => {
- * //   a: 123,
- * //   b: 145.67,
- * //   c: 'test中文1',
- * //   d: true,
- * //   e: [Function: e],
- * //   f: [ 11, 'xyz', false, Uint8Array(3) [ 166, 197, 215 ] ],
- * //   g: {
- * //     ga: 223,
- * //     gb: 245.67,
- * //     gc: 'test中文2',
- * //     gd: Uint8Array(3) [ 66, 97, 115 ]
- * //   }
- * // }
- *
- */
-function treeObj(data, hookFun = null) {
-    let p = {}
-    let nowKey = ['pnode']
+function traverseObj(data, f) {
+    let nowKey = []
 
     //check
-    if (!isFunction(hookFun)) {
-        hookFun = (value) => {
-            return value
+    if (!isarr(data) && !isobj(data)) {
+        data = f(data, null, [])
+        return data
+    }
+
+    function getKeys(obj) {
+        if (isarr(obj)) {
+            //return keys(obj)
+            return range(size(obj))
+        }
+        if (isobj(obj)) {
+            let v1 = Object.getOwnPropertyNames(obj)
+            // console.log('v1', v1)
+            let v2 = Object.getOwnPropertySymbols(obj)
+            // console.log('v2', v2)
+            if (!isarr(v1)) {
+                v1 = []
+            }
+            if (!isarr(v2)) {
+                v2 = []
+            }
+            return [...v1, ...v2]
+        }
+        return obj
+    }
+
+    function traverse(vs, _k) {
+        //console.log('vs', vs)
+        if (isarr(vs)) {
+            let ks = getKeys(vs)
+            return ks.map((k) => {
+                let v = vs[k]
+                //console.log('map', v, k)
+                let t = cloneDeep(nowKey)
+                nowKey.push(k)
+                v = f(v, k, t)
+                v = traverse(v, k)
+                nowKey.pop()
+                return v
+            })
+        }
+        else if (isobj(vs)) {
+            let ks = getKeys(vs)
+            let o = {}
+            ks.map((k) => {
+                let v = vs[k]
+                //console.log('map', v, k)
+                let t = cloneDeep(nowKey)
+                nowKey.push(k)
+                v = f(v, k, t)
+                v = traverse(v, k)
+                nowKey.pop()
+                o[k] = v
+            })
+            return o
+        }
+        else {
+            return vs //f(vs, _k, cloneDeep(slice(nowKey, 1)))
         }
     }
+
+    return traverse(data, f)
+}
+
+
+function runObj(data, hookFun) {
+    let p = {}
+    let nowKey = ['pnode']
 
     //列舉pArray內元素
     function pArray({ value }) {
@@ -190,6 +191,99 @@ function treeObj(data, hookFun = null) {
     }
 
     return p.pnode
+}
+
+
+/**
+ * 遍歷物件並回傳複製物件，類似JSON.stringify，但不會序列化成為字串
+ *
+ * Unit Test: {@link https://github.com/yuda-lyu/wsemi/blob/master/test/treeObj.test.js Github}
+ * @memberOf wsemi
+ * @param {*} data 輸入任意資料
+ * @param {Function} [hookFun=null] 輸入攔截處理函數，預設null
+ * @param {Object} [opt={}] 輸入設定物件，預設{}
+ * @param {Boolean} [opt.force=false] 輸入是否可遍歷Symbol鍵值功能布林值，給true時可處理key為Symbol之情形，預設false
+ * @returns {*} 回傳複製後的任意資料
+ * @example
+ *
+ * let data = {
+ *     a: 123,
+ *     b: 145.67,
+ *     c: 'test中文1',
+ *     d: true,
+ *     e: function() {},
+ *     f: [11, 'xyz', false, new Uint8Array([166, 197, 215])],
+ *     g: {
+ *         ga: 223,
+ *         gb: 245.67,
+ *         gc: 'test中文2',
+ *         gd: new Uint8Array([66, 97, 115]),
+ *     },
+ * }
+ * let r = treeObj(data, (value, key, nk) => {
+ *     console.log('=>', value, key, nk)
+ *     return value
+ * })
+ * console.log('r =>', r)
+ * // => 123 a []
+ * // => 145.67 b []
+ * // => test中文1 c []
+ * // => true d []
+ * // => [Function: e] e []
+ * // => [ 11, 'xyz', false, Uint8Array(3) [ 166, 197, 215 ] ] f []
+ * // => 11 0 [ 'f' ]
+ * // => xyz 1 [ 'f' ]
+ * // => false 2 [ 'f' ]
+ * // => Uint8Array(3) [ 166, 197, 215 ] 3 [ 'f' ]
+ * // => {
+ * //   ga: 223,
+ * //   gb: 245.67,
+ * //   gc: 'test中文2',
+ * //   gd: Uint8Array(3) [ 66, 97, 115 ]
+ * // } g []
+ * // => 223 ga [ 'g' ]
+ * // => 245.67 gb [ 'g' ]
+ * // => test中文2 gc [ 'g' ]
+ * // => Uint8Array(3) [ 66, 97, 115 ] gd [ 'g' ]
+ * // r => {
+ * //   a: 123,
+ * //   b: 145.67,
+ * //   c: 'test中文1',
+ * //   d: true,
+ * //   e: [Function: e],
+ * //   f: [ 11, 'xyz', false, Uint8Array(3) [ 166, 197, 215 ] ],
+ * //   g: {
+ * //     ga: 223,
+ * //     gb: 245.67,
+ * //     gc: 'test中文2',
+ * //     gd: Uint8Array(3) [ 66, 97, 115 ]
+ * //   }
+ * // }
+ *
+ */
+function treeObj(data, hookFun = null, opt = {}) {
+
+    //hookFun
+    if (!isFunction(hookFun)) {
+        hookFun = (value) => {
+            return value
+        }
+    }
+
+    //force
+    let force = get(opt, 'force', false)
+    if (!isbol(force)) {
+        force = false
+    }
+
+    //call
+    if (force) {
+        return traverseObj(data, hookFun)
+    }
+    else {
+        return runObj(data, hookFun)
+    }
+
 }
 
 
--- test/treeObj.test.js
+++ test/treeObj.test.js
@@ -8,11 +8,13 @@ describe(`treeObj`, function() {
         b: {
             c: new Uint8Array([66, 97, 115]),
         },
+        d: Symbol('foo'),
+        [Symbol('sym')]: 'e',
     }
-    let cdata = `{ a: [123, 45.67, 'test中文'], b: { c: new Uint8Array([66, 97, 115]) } }`
-    let cres = `[{"value":[123,45.67,"test中文"],"key":"a","nk":[]},{"value":123,"key":0,"nk":["a"]},{"value":45.67,"key":1,"nk":["a"]},{"value":"test中文","key":2,"nk":["a"]},{"value":{"c":{"0":66,"1":97,"2":115}},"key":"b","nk":[]},{"value":{"0":66,"1":97,"2":115},"key":"c","nk":["b"]},{"a":[123,45.67,"test中文"],"b":{"c":{"0":66,"1":97,"2":115}}}]`
 
-    it(`should return ${cres} when input ${cdata}`, function() {
+    let cdata1 = `{ a: [123, 45.67, 'test中文'], b: { c: new Uint8Array([66, 97, 115]) } }`
+    let cres1 = `[{"value":[123,45.67,"test中文"],"key":"a","nk":[]},{"value":123,"key":0,"nk":["a"]},{"value":45.67,"key":1,"nk":["a"]},{"value":"test中文","key":2,"nk":["a"]},{"value":{"c":{"0":66,"1":97,"2":115}},"key":"b","nk":[]},{"value":{"0":66,"1":97,"2":115},"key":"c","nk":["b"]},{"key":"d","nk":[]},{"a":[123,45.67,"test中文"],"b":{"c":{"0":66,"1":97,"2":115}}}]`
+    it(`should return ${cres1} when input ${cdata1}`, function() {
         let m = []
         let r = treeObj(data, (value, key, nk) => {
             m.push({ value, key, nk })
@@ -20,7 +22,21 @@ describe(`treeObj`, function() {
         })
         m.push(r)
         r = JSON.stringify(m)
-        let rr = cres
+        let rr = cres1
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    let cdata2 = `{ a: [123, 45.67, 'test中文'], b: { c: new Uint8Array([66, 97, 115]) } }, [callback function], { force: true }`
+    let cres2 = `[{"value":[123,45.67,"test中文"],"key":"a","nk":[]},{"value":123,"key":0,"nk":["a"]},{"value":45.67,"key":1,"nk":["a"]},{"value":"test中文","key":2,"nk":["a"]},{"value":{"c":{"0":66,"1":97,"2":115}},"key":"b","nk":[]},{"value":{"0":66,"1":97,"2":115},"key":"c","nk":["b"]},{"key":"d","nk":[]},{"value":"e","nk":[]},{"a":[123,45.67,"test中文"],"b":{"c":{"0":66,"1":97,"2":115}}}]`
+    it(`should return ${cres2} when input ${cdata2}`, function() {
+        let m = []
+        let r = treeObj(data, (value, key, nk) => {
+            m.push({ value, key, nk })
+            return value
+        }, { force: true })
+        m.push(r)
+        r = JSON.stringify(m)
+        let rr = cres2
         assert.strict.deepStrictEqual(r, rr)
     })
 
