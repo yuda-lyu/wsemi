46cce604b7a531c8db14f70b8174b58e005a4814
yuda-lyu <firsemisphere@gmail.com>
2021-05-19 10:56:02 +0800

    modify: trim exam.

--- src/trim.mjs
+++ src/trim.mjs
@@ -3,8 +3,7 @@ import isestr from './isestr.mjs'
 
 
 /**
- * 字串頭尾去除空白字串
- * 若輸入不是字串時則回傳空字串
+ * 字串頭尾去除空白字串，若字串開頭含有BOM亦可清除，若輸入不是字串時則回傳空字串
  *
  * Unit Test: {@link https://github.com/yuda-lyu/wsemi/blob/master/test/trim.test.js Github}
  * @memberOf wsemi
@@ -14,6 +13,32 @@ import isestr from './isestr.mjs'
  *
  * console.log(trim(' abc 中 文 '))
  * // => 'abc 中 文'
+ *
+ * let tr = (c) => {
+ *     c = replace(c, '\r\n', '')
+ *     c = replace(c, '\r', '')
+ *     c = replace(c, '\n', '')
+ *     return c
+ * }
+ *
+ * let b64BOM = '77u/YSxiLCzkuK3mlocNCjEsMjMuNDUsImFiYyIsIjY3LjgiDQo='
+ * let b64NoBOM = 'YSxiLCzkuK3mlocNCjEsMjMuNDUsImFiYyIsIjY3LjgiDQo='
+ * let cBOMCor = `
+ * a,b,,中文
+ * 1,23.45,"abc","67.8"
+ * `
+ *
+ * let cBOM = b642str(b64BOM)
+ * console.log(tr(cBOM) === tr(cBOMCor))
+ * // => false
+ *
+ * let cBOMtrim = trim(cBOM)
+ * console.log(tr(cBOMtrim) === tr(cBOMCor))
+ * // => true
+ *
+ * let cNoBOM = b642str(b64NoBOM)
+ * console.log(tr(cNoBOM) === tr(cBOMCor))
+ * // => true
  *
  */
 function trim(c) {
