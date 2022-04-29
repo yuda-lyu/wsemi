076e968c777413eb2666ccdeddee10c6b5312409
yuda-lyu <firsemisphere@gmail.com>
2022-04-29 12:14:39 +0800

    modify: add domGetWindowSize

---
+++ src/domGetWindowSize.mjs
@@ -0,0 +1,63 @@
+import get from 'lodash/get'
+import filter from 'lodash/filter'
+import min from 'lodash/min'
+import isNumber from 'lodash/isNumber'
+import getGlobal from './getGlobal.mjs'
+
+
+/**
+ * 前端取得window內實際顯示寬度與高度
+ *
+ * Unit Test: {@link https://github.com/yuda-lyu/wsemi/blob/master/test/domGetWindowSize.test.mjs Github}
+ * @memberOf wsemi
+ * @param {HTMLElement} ele 輸入DOM元素
+ * @returns {Object} 回傳getBoundingClientRect結果物件
+ * @example
+ * need test in browser
+ *
+ * let r = domGetWindowSize()
+ * console.log(r)
+ * // => {
+ *   width:...,
+ *   height:...,
+ * }
+ *
+ */
+function domGetWindowSize() {
+
+    //getGlobal
+    let g = getGlobal()
+
+    //domGetWindowWidth
+    let domGetWindowWidth = () => {
+        let w1 = get(g, 'innerWidth')
+        let w2 = get(g, 'document.documentElement.clientWidth')
+        let w3 = get(g, 'document.body.clientWidth')
+        let ws = [w1, w2, w3]
+        ws = filter(ws, isNumber)
+        let w = min(ws)
+        return w
+    }
+
+    //domGetWindowHeight
+    let domGetWindowHeight = () => {
+        let h1 = get(g, 'innerHeight')
+        let h2 = get(g, 'document.documentElement.clientHeight')
+        let h3 = get(g, 'document.body.clientHeight')
+        let hs = [h1, h2, h3]
+        hs = filter(hs, isNumber)
+        let h = min(hs)
+        return h
+    }
+
+    //s
+    let s = {
+        width: domGetWindowWidth(),
+        height: domGetWindowHeight(),
+    }
+
+    return s
+}
+
+
+export default domGetWindowSize
---
+++ test/domGetWindowSize.test.mjs
@@ -0,0 +1,11 @@
+import assert from 'assert'
+//import domGetWindowSize from '../src/domGetWindowSize.mjs'
+
+
+describe(`domGetWindowSize`, function() {
+
+    it(`need test in browser`, function() {
+        assert.strict.deepStrictEqual(1, 1)
+    })
+
+})
