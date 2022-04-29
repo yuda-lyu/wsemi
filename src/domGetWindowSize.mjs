d658f74bcc9eab1b510d674586c64c9519a06c24
yuda-lyu <firsemisphere@gmail.com>
2022-04-29 12:15:31 +0800

    modify: msg

--- src/domGetWindowSize.mjs
+++ src/domGetWindowSize.mjs
@@ -10,8 +10,7 @@ import getGlobal from './getGlobal.mjs'
  *
  * Unit Test: {@link https://github.com/yuda-lyu/wsemi/blob/master/test/domGetWindowSize.test.mjs Github}
  * @memberOf wsemi
- * @param {HTMLElement} ele 輸入DOM元素
- * @returns {Object} 回傳getBoundingClientRect結果物件
+ * @returns {Object} 回傳window內實際顯示寬度與高度物件
  * @example
  * need test in browser
  *
