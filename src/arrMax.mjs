530d2303595602968eaa4c1460074605c535cb27
yuda-lyu <firsemisphere@gmail.com>
2022-09-21 17:18:48 +0800

    modify: add arrMax

---
+++ src/arrMax.mjs
@@ -0,0 +1,140 @@
+import get from 'lodash/get'
+import map from 'lodash/map'
+import size from 'lodash/size'
+import isearr from './isearr.mjs'
+import isestr from './isestr.mjs'
+import isbol from './isbol.mjs'
+import isnum from './isnum.mjs'
+import cdbl from './cdbl.mjs'
+
+
+/**
+ * 找尋陣列內元素最大值
+ *
+ * Unit Test: {@link https://github.com/yuda-lyu/wsemi/blob/master/test/arrMax.test.mjs Github}
+ * @memberOf wsemi
+ * @param {Array} vall 輸入原始陣列
+ * @returns {Number|Object} 回傳最大值或判斷具有最大值之物件
+ * @example
+ *
+ * let r
+ *
+ * r = arrMax([1, 30, 4, 100000, 21])
+ * console.log(r)
+ * // => 100000
+ *
+ * r = arrMax([1, 30, 4, 100000, 21], { returnIndex: true })
+ * console.log(r)
+ * // => 3
+ *
+ * r = arrMax(['March', 'Jan', 'Feb', 'Dec'])
+ * console.log(r)
+ * // => null
+ *
+ * r = arrMax(['1', '30', '  4  ', '100000', '21'])
+ * console.log(r)
+ * // => 100000
+ *
+ * r = arrMax(['1', '30', '  4  ', '100000', 21])
+ * console.log(r)
+ * // => 100000
+ *
+ * r = arrMax(['a1', 'b30', '  4  ', '100000', 21])
+ * console.log(r)
+ * // => 100000
+ *
+ * r = arrMax(
+ *     [{ s: 'March', i: 1, }, { s: 'Jan', i: 4, }, { s: 'Feb', i: 100000, }, { s: 'Dec', i: 30, }, { s: 'Nov', i: 'a25', }],
+ *     { compareKey: 'i' }
+ * )
+ * console.log(r)
+ * // => { s: 'Feb', i: 100000 }
+ *
+ * r = arrMax(
+ *     [{ s: 'March', i: 1, }, { s: 'Jan', i: 4, }, { s: 'Feb', i: 100000, }, { s: 'Dec', i: 30, }, { s: 'Nov', i: 'a25', }],
+ *     { compareKey: 's' }
+ * )
+ * console.log(r)
+ * // => null
+ *
+ * r = arrMax(
+ *     [{ s: 'March', i: 1, }, { s: 'Jan', i: 4, }, { s: 'Feb', i: 100000, }, { s: 'Dec', i: 30, }, { s: 'Nov', i: 'a25', }],
+ *     { compareKey: 'i', returnIndex: true }
+ * )
+ * console.log(r)
+ * // => 2
+ *
+ */
+function arrMax(vall, opt = {}) {
+
+    //check
+    if (!isearr(vall)) {
+        return null
+    }
+
+    //returnIndex
+    let returnIndex = get(opt, 'returnIndex')
+    if (!isbol(returnIndex)) {
+        returnIndex = false
+    }
+
+    //compareKey
+    let compareKey = get(opt, 'compareKey', null)
+
+    //vallTrans
+    let vallTrans = vall
+    if (isestr(compareKey)) {
+        vallTrans = map(vall, (v, k) => {
+            return get(v, compareKey, '')
+        })
+    }
+
+    //find
+    let ind = -1
+    let vv = null
+    for (let k = 0; k < size(vallTrans); k++) {
+
+        //v
+        let v = vallTrans[k]
+
+        //cdbl
+        if (isnum(v)) {
+            v = cdbl(v)
+        }
+        else {
+            continue
+        }
+
+        //init vv
+        if (vv === null && isnum(v)) {
+            vv = v
+            continue
+        }
+
+        //cmp
+        if (vv < v) {
+            vv = v
+            ind = k
+        }
+
+    }
+
+    //compareKey
+    if (isestr(compareKey) && ind >= 0) {
+        vv = vall[ind]
+    }
+
+    //returnIndex
+    let r = null
+    if (returnIndex) {
+        r = ind
+    }
+    else {
+        r = vv
+    }
+
+    return r
+}
+
+
+export default arrMax
---
+++ test/arrMax.test.mjs
@@ -0,0 +1,220 @@
+import assert from 'assert'
+import arrMax from '../src/arrMax.mjs'
+
+
+describe(`arrMax`, function() {
+    let k
+    let oin = {}
+    let oout = {}
+
+    k = 1
+    oin[k] = [1, 30, 4, 100000, 21]
+    oout[k] = 100000
+    it(`should return ${JSON.stringify(oout[k])} when input ${JSON.stringify(oin[k])}`, function() {
+        k = 1
+        let r = arrMax(oin[k])
+        let rr = oout[k]
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    k = 2
+    oin[k] = [1, 30, 4, 100000, 21]
+    oout[k] = 3
+    it(`should return ${JSON.stringify(oout[k])} when input ${JSON.stringify(oin[k])}, { returnIndex: true }`, function() {
+        k = 2
+        let r = arrMax(oin[k], { returnIndex: true })
+        let rr = oout[k]
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    k = 3
+    oin[k] = ['March', 'Jan', 'Feb', 'Dec']
+    oout[k] = null
+    it(`should return ${JSON.stringify(oout[k])} when input ${JSON.stringify(oin[k])}`, function() {
+        k = 3
+        let r = arrMax(oin[k])
+        let rr = oout[k]
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    k = 4
+    oin[k] = ['1', '30', '  4  ', '100000', '21']
+    oout[k] = 100000
+    it(`should return ${JSON.stringify(oout[k])} when input ${JSON.stringify(oin[k])}`, function() {
+        k = 4
+        let r = arrMax(oin[k])
+        let rr = oout[k]
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    k = 5
+    oin[k] = ['1', '30', '  4  ', '100000', 21]
+    oout[k] = 100000
+    it(`should return ${JSON.stringify(oout[k])} when input ${JSON.stringify(oin[k])}`, function() {
+        k = 5
+        let r = arrMax(oin[k])
+        let rr = oout[k]
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    k = 6
+    oin[k] = ['a1', 'b30', '  4  ', '100000', 21]
+    oout[k] = 100000
+    it(`should return ${JSON.stringify(oout[k])} when input ${JSON.stringify(oin[k])}`, function() {
+        k = 6
+        let r = arrMax(oin[k])
+        let rr = oout[k]
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    k = 7
+    oin[k] = [{ s: 'March', i: 1, }, { s: 'Jan', i: 4, }, { s: 'Feb', i: 100000, }, { s: 'Dec', i: 30, }, { s: 'Nov', i: 'a25', }]
+    oout[k] = { s: 'Feb', i: 100000 }
+    it(`should return ${JSON.stringify(oout[k])} when input ${JSON.stringify(oin[k])}, { compareKey: 'i' }`, function() {
+        k = 7
+        let r = arrMax(oin[k], { compareKey: 'i' })
+        let rr = oout[k]
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    k = 8
+    oin[k] = [{ s: 'March', i: 1, }, { s: 'Jan', i: 4, }, { s: 'Feb', i: 100000, }, { s: 'Dec', i: 30, }, { s: 'Nov', i: 'a25', }]
+    oout[k] = null
+    it(`should return ${JSON.stringify(oout[k])} when input ${JSON.stringify(oin[k])}, { compareKey: 's' }`, function() {
+        k = 8
+        let r = arrMax(oin[k], { compareKey: 's' })
+        let rr = oout[k]
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    k = 9
+    oin[k] = [{ s: 'March', i: 1, }, { s: 'Jan', i: 4, }, { s: 'Feb', i: 100000, }, { s: 'Dec', i: 30, }, { s: 'Nov', i: 'a25', }]
+    oout[k] = 2
+    it(`should return ${JSON.stringify(oout[k])} when input ${JSON.stringify(oin[k])}, { compareKey: 'i', returnIndex: true }`, function() {
+        k = 9
+        let r = arrMax(oin[k], { compareKey: 'i', returnIndex: true })
+        let rr = oout[k]
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input ['123'], ''`, function() {
+        let r = arrMax(['123'], '')
+        let rr = 123
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input ['123'], []`, function() {
+        let r = arrMax(['123'], [])
+        let rr = 123
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input ['123'], {}`, function() {
+        let r = arrMax(['123'], {})
+        let rr = 123
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input ['123'], null`, function() {
+        let r = arrMax(['123'], null)
+        let rr = 123
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input ['123'], undefined`, function() {
+        let r = arrMax(['123'], undefined)
+        let rr = 123
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input ['abc'], ''`, function() {
+        let r = arrMax(['abc'], '')
+        let rr = null
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input ['abc'], []`, function() {
+        let r = arrMax(['abc'], [])
+        let rr = null
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input ['abc'], {}`, function() {
+        let r = arrMax(['abc'], {})
+        let rr = null
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input ['abc'], null`, function() {
+        let r = arrMax(['abc'], null)
+        let rr = null
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input ['abc'], undefined`, function() {
+        let r = arrMax(['abc'], undefined)
+        let rr = null
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input [], ''`, function() {
+        let r = arrMax([], '')
+        let rr = null
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input [], []`, function() {
+        let r = arrMax([], [])
+        let rr = null
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input [], {}`, function() {
+        let r = arrMax([], {})
+        let rr = null
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input [], null`, function() {
+        let r = arrMax([], null)
+        let rr = null
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input [], undefined`, function() {
+        let r = arrMax([], undefined)
+        let rr = null
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input ''`, function() {
+        let r = arrMax('')
+        let rr = null
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input []`, function() {
+        let r = arrMax([])
+        let rr = null
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input {}`, function() {
+        let r = arrMax({})
+        let rr = null
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input null`, function() {
+        let r = arrMax(null)
+        let rr = null
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+    it(`should return [] when input undefined`, function() {
+        let r = arrMax(undefined)
+        let rr = null
+        assert.strict.deepStrictEqual(r, rr)
+    })
+
+})
