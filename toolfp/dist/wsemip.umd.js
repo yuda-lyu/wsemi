f17302a4521c3112ff34b4b79ad7e8452f06d192
yuda-lyu <firsemisphere@gmail.com>
2025-06-14 19:24:17 +0800

    modify: ext. opt in preciseNum

--- toolfp/dist/wsemip.umd.js
+++ toolfp/dist/wsemip.umd.js
@@ -1,5 +1,5 @@
 /*!
- * wsemip v1.7.94
+ * wsemip v1.8.0
  * (c) 2018-2021 yuda-lyu(semisphere)
  * Released under the MIT License.
  */
--- toolparfor/dist/wpf.umd.js
+++ toolparfor/dist/wpf.umd.js
@@ -1,5 +1,5 @@
 /*!
- * wpf v1.7.94
+ * wpf v1.8.0
  * (c) 2018-2021 yuda-lyu(semisphere)
  * Released under the MIT License.
  */
