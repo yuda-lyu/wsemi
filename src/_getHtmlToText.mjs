4612a1799ba112f7acdef74966cf7ef84f950f47
yuda-lyu <firsemisphere@gmail.com>
2026-06-22 14:26:27 +0800

    fix: _getHtmlToText

--- src/_getHtmlToText.mjs
+++ src/_getHtmlToText.mjs
@@ -10,29 +10,33 @@ async function getHtmlToText() {
     //nodejs須使用(* as ht)才能取得全部
     //因不打包, 瀏覽器端須先引用script再由window.ht取得
 
-    if (Object.prototype.toString.call(ht) === '[object Module]') { //於nodejs會顯示module, 因不打包瀏覽器端會顯示object
+    let keyFun = 'htmlToText'
+
+    //直接探測import進來的ht是否具備htmlToText, 不靠Object.prototype.toString的'[object Module]'判斷
+    //(打包後bundler interop產生的namespace為無Symbol.toStringTag的純物件, toString回'[object Object]'會誤判)
+    //可涵蓋nodejs native ESM namespace, 以及bundler interop後htmlToText為複製getter或掛在default之情形
+    if (isfun(get(ht, keyFun))) {
         return ht
+    }
+    if (isfun(get(ht, ['default', keyFun]))) {
+        return ht.default
     }
 
     let g = globalThis
     // let g = getGlobal()
 
-    let _g_ht_default = get(g, 'ht.default', null)
-
+    //fallback: 因html-to-text於瀏覽器端沒有umd版只有esm, 故使用import再掛入window為defer會有延遲載入問題, 得要偵測等待
     let _g_ht = null
     await waitFun(() => {
         _g_ht = get(g, 'ht', null)
-        return iseobj(_g_ht) //因html-to-text於瀏覽器端沒有umd版只有esm, 故使用import再掛入window為defer會有延遲載入問題, 得要偵測等待
-    }, { timeInterval: 50 })
+        return iseobj(_g_ht)
+    }, { attemptNum: 4000, timeInterval: 50 })
 
-    let keyFun = 'htmlToText'
-
-    if (iseobj(_g_ht_default) && isfun(_g_ht_default[keyFun])) {
-        return _g_ht_default
-    }
-
-    if (iseobj(_g_ht) && isfun(_g_ht[keyFun])) {
+    if (isfun(get(_g_ht, keyFun))) {
         return _g_ht
+    }
+    if (isfun(get(_g_ht, ['default', keyFun]))) {
+        return _g_ht.default
     }
 
     console.log('ht', ht, 'g.ht', g.ht)
--- toolfp/dist/wsemip.umd.js
+++ toolfp/dist/wsemip.umd.js
@@ -1,5 +1,5 @@
 /*!
- * wsemip v1.8.60
+ * wsemip v1.8.61
  * (c) 2018-2021 yuda-lyu(semisphere)
  * Released under the MIT License.
  */
