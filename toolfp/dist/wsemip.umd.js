5f99f5407b5b011ad09c053bdd939a15e3cd4712
yuda-lyu <firsemisphere@gmail.com>
2026-08-21 10:47:34 +0800

    modify: update pks

--- toolfp/dist/wsemip.umd.js
+++ toolfp/dist/wsemip.umd.js
@@ -1,5 +1,5 @@
 /*!
- * wsemip v1.8.78
+ * wsemip v1.8.79
  * (c) 2018-2021 yuda-lyu(semisphere)
  * Released under the MIT License.
  */
--- toolparfor/dist/wpf_pmSeries.umd.js
+++ toolparfor/dist/wpf_pmSeries.umd.js
@@ -1,5 +1,5 @@
 /*!
- * wpf_pmSeries v1.8.78
+ * wpf_pmSeries v1.8.79
  * (c) 2018-2021 yuda-lyu(semisphere)
  * Released under the MIT License.
  */
